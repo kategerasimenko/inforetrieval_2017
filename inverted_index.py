64a9e3c3c11ea2c3c397ba0783ed599fc91e3653
kategerasimenko <katgerasimenko@gmail.com>
2017-09-21 09:21:52 +0400

    fixed bug

--- inverted_index.py
+++ inverted_index.py
@@ -3,7 +3,9 @@ from collections import defaultdict
 def inv_index(collection):
     inv_idx = defaultdict(list)
     for i,c in enumerate(collection):
+        c = set(c)
         for word in c:
             inv_idx[word].append(i+1)
     return inv_idx
+
 
